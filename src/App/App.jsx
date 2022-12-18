c50d7d681fe1289cfb8dbf1cd03dc0500d112ba1
ivanlazhko <ivanblazhko44@gmail.com>
2022-12-18 16:51:07 +0200

    hw-04-Phonebook

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,88 +1,70 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
 
-import PhonebookDefaultData from './Data/PhonebookDefaultData.json';
+import Style from './Style/Phonebook.module.css';
+import PhonebookForm from './Components/PhonebookForm';
+import PhonebookSearch from './Components/PhonebookSearch';
+import PhonebookContacts from './Components/PhonebookContacts';
 
-import Style from './Style/Phonebook.module.css'
-import PhonebookForm from "./Components/PhonebookForm";
-import PhonebookSearch from "./Components/PhonebookSearch";
-import PhonebookContacts from "./Components/PhonebookContacts";
+const App = () => {
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) || []
+  );
+  const [search, setSearch] = useState('');
 
-class App extends Component {
-  state = {
-    contacts: [...PhonebookDefaultData],
-    search: '',
-  };
-  addContact = (name, phone) => {
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = (name, phone) => {
     const newContact = {
       id: nanoid(),
       name,
       phone,
     };
-    const isExist = this.state.contacts.find(i => i.name === name)
+    const isExist = contacts.find(i => i.name === name);
     if (isExist) {
       return alert(name + ' is already in contacts');
     }
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
+    setContacts(prevState => [...prevState, newContact]);
   };
-  componentDidMount() {
-    const storage = localStorage.getItem("contacts")
-    const parsedContacts = JSON.parse(storage)
-    if (parsedContacts) {
-      this.setState({
-        contacts: parsedContacts
-      })
-    }
-  }
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem("contacts", JSON.stringify(this.state.contacts))
-    }
-  }
-  onDeleteContact = Id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== Id),
-    }));
+
+  const onDeleteContact = Id => {
+    setContacts(prevState => prevState.filter(contact => contact.id !== Id));
   };
-  getVisibleContacts = () => {
-    const { search, contacts } = this.state;
+
+  const getVisibleContacts = () => {
     const normalizeFilter = search.toLowerCase();
     return contacts.filter(todo =>
       todo.name.toLowerCase().includes(normalizeFilter)
     );
   };
-  handelSearchValue = event => {
-    this.setState({
-      search: event.currentTarget.value,
-    });
+
+  const handelSearchValue = event => {
+    setSearch(event.currentTarget.value);
   };
-  render() {
-    const { search } = this.state;
-    const visibleContacts = this.getVisibleContacts();
-    return (
-      <div className={Style.app__wrapper}>
-        <div className={Style.phonebook__body}>
-          <h1 className={Style.phonebook__title}>Phonebook</h1>
-          <div>
-            <PhonebookForm
-              addContact={this.addContact}
-            />
-            <h2 className={Style.phonebook__title}>Contacts</h2>
-            <PhonebookSearch
-              stateSearch={search}
-              handelSearchValue={this.handelSearchValue}
-            />
-            <PhonebookContacts
-              visibleContacts={visibleContacts}
-              onDeleteContact={this.onDeleteContact}
-            />
-          </div>
+
+  const visibleContacts = getVisibleContacts();
+
+  return (
+    <div className={Style.app__wrapper}>
+      <div className={Style.phonebook__body}>
+        <h1 className={Style.phonebook__title}>Phonebook</h1>
+        <div>
+          <PhonebookForm addContact={addContact} />
+          <h2 className={Style.phonebook__title}>Contacts</h2>
+          <PhonebookSearch
+            stateSearch={search}
+            handelSearchValue={handelSearchValue}
+          />
+          <PhonebookContacts
+            onDeleteContact={onDeleteContact}
+            visibleContacts={visibleContacts}
+          />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
--- src/App/Components/PhonebookForm.jsx
+++ src/App/Components/PhonebookForm.jsx
@@ -1,66 +1,69 @@
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import Style from '../Style/Phonebook.module.css';
-import {Component} from "react";
 
-class PhonebookForm extends Component {
-  state = {
-    name: '',
-    tel: '',
-  }
-  handelInputValue = event => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value,
-    });
+import Style from '../Style/Phonebook.module.css';
+
+const PhonebookForm = ({ addContact }) => {
+  const [name, setName] = useState('');
+  const [tel, setTel] = useState('');
+
+  const handelInputValue = event => {
+    if (event.currentTarget.name === 'name') {
+      return setName(event.currentTarget.value);
+    }
+    if (event.currentTarget.name === 'tel') {
+      return setTel(event.currentTarget.value);
+    }
   };
-  handelSubmit = event => {
+
+  const handelSubmit = event => {
     event.preventDefault();
-    this.props.addContact(this.state.name, this.state.tel);
-    this.reset();
+    addContact(name, tel);
+    reset();
   };
-  reset = () => {
-    this.setState({ name: '', tel: '' });
+
+  const reset = () => {
+    setName('');
+    setTel('');
   };
-  render() {
-    const { tel, name } = this.state
-    return (
-      <form onSubmit={this.handelSubmit}>
-        <label htmlFor="" className={Style.phonebook__text}>
-          Name
-          <input
-            type="text"
-            name="name"
-            onChange={this.handelInputValue}
-            value={name}
-            className={Style.phonebook__input}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-        </label>
-        <label htmlFor="" className={Style.phonebook__text}>
-          Phone
-          <input
-            onChange={this.handelInputValue}
-            value={tel}
-            className={Style.phonebook__input}
-            type="tel"
-            name="tel"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-        </label>
-        <button type="submit" className={Style.phonebook__btn}>
-          Add contact
-        </button>
-      </form>
-    );
-  }
-}
+
+  return (
+    <form onSubmit={handelSubmit}>
+      <label htmlFor="" className={Style.phonebook__text}>
+        Name
+        <input
+          type="text"
+          name="name"
+          onChange={handelInputValue}
+          value={name}
+          className={Style.phonebook__input}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </label>
+      <label htmlFor="" className={Style.phonebook__text}>
+        Phone
+        <input
+          onChange={handelInputValue}
+          value={tel}
+          className={Style.phonebook__input}
+          type="tel"
+          name="tel"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </label>
+      <button type="submit" className={Style.phonebook__btn}>
+        Add contact
+      </button>
+    </form>
+  );
+};
 
 export default PhonebookForm;
 
 PhonebookForm.propTypes = {
-  addContact: PropTypes.func
+  addContact: PropTypes.func,
 };
